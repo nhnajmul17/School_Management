fe54fd21f0cd596f2551e7e2129b761d5fad2a6f
plcpl-hasan <najmul.hasan@profecialinks.com>
2022-09-24 20:11:32 +0600

    First Test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          school management <code>by</code> sadik and bros.
         </p>
         <a
           className="App-link"
